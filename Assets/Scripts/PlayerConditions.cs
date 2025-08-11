c69f70c836378bbe3bb82ca77c2281d4f68feabe
0time12 <onr55@naver.com>
2025-08-11 09:05:45 +0900

    Initial commit

---
+++ Assets/Scripts/PlayerConditions.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class PlayerCondition : MonoBehaviour
+{
+    public UICondition uiCondition;
+
+    Condition health { get { return uiCondition.health; } }
+    Condition stamina { get { return uiCondition.stamina; } }
+
+    public event Action onTakeDamage;
+
+    private void Update()
+    {
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Die()
+    {
+        Debug.Log("Die");
+    }
+}
