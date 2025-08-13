36e53e310d6dcc5bd2d498b8e0b1455183dea902
0time12 <onr55@naver.com>
2025-08-13 14:27:02 +0900

    [Fix] 탐지 범위 조정

--- Assets/Scripts/TrapEntrance.cs
+++ Assets/Scripts/TrapEntrance.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 
 public class TrapEntrance : MonoBehaviour
 {
-    public float detectionDistance = 3f;
-    public float knockbackForce = 1000000f;
-    
+    public float detectionDistance = 5f;
+    public float knockbackForce = 500f;
+
     private GameObject player;
     private Rigidbody playerRigidbody;
     private bool isPlayerDetected = false;
@@ -26,14 +26,21 @@ public class TrapEntrance : MonoBehaviour
         if (player == null) return;
 
         Vector3 directionToPlayer = (player.transform.position - transform.position).normalized;
-        Ray ray = new Ray(transform.position, directionToPlayer);
+        Vector3 rayStartPoint = transform.position + directionToPlayer * 0.5f;
+        Vector3 rayDirection = (player.transform.position - rayStartPoint).normalized;
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, detectionDistance))
+        Debug.DrawRay(rayStartPoint, rayDirection * detectionDistance, Color.red);
+
+        if (Physics.Raycast(rayStartPoint, rayDirection, out hit, detectionDistance))
         {
             if (hit.collider.CompareTag("Player"))
             {
                 isPlayerDetected = true;
+            }
+            else
+            {
+                isPlayerDetected = false;
             }
         }
         else
@@ -46,20 +53,14 @@ public class TrapEntrance : MonoBehaviour
     {
         if (isPlayerDetected && playerRigidbody != null)
         {
-            playerRigidbody.velocity = Vector3.zero;
+            playerRigidbody.velocity = new Vector3(0, playerRigidbody.velocity.y, 0);
 
-            // 수평 방향 벡터를 계산하고 Y축 힘을 제거합니다.
-            Vector3 horizontalDirection = (player.transform.position - transform.position);
-            horizontalDirection.y = 0;
-            horizontalDirection.Normalize();
+            // 튕겨나가는 방향
+            Vector3 knockbackDirection = (player.transform.position - transform.position);
+            knockbackDirection.y = 0; 
+            knockbackDirection.Normalize();
 
-            // 수평 방향으로 튕겨나가는 힘을 가합니다.
-            playerRigidbody.AddForce(horizontalDirection * knockbackForce, ForceMode.Impulse);
-
-            // 위로 튕겨나가는 힘을 별도로 가합니다.
-            // verticalKnockbackForce 값을 원하는 대로 조절해보세요.
-            float verticalKnockbackForce = 200f;
-            playerRigidbody.AddForce(Vector3.up * verticalKnockbackForce, ForceMode.Impulse);
+            playerRigidbody.AddForce(knockbackDirection * knockbackForce, ForceMode.Impulse);
 
             isPlayerDetected = false;
         }
